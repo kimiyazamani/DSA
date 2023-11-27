2b0b4b78b651bf16862514aa9d2299e42c0cb2e2
kimiya zamani <zamanikimiya.2001@gmail.com>
2023-11-27 16:12:21 +0330

    peek

--- stack/stack.py
+++ stack/stack.py
@@ -4,6 +4,10 @@ class Stack:
         self.List = [None] * size
         self.top = 0
 
+    def size(self):
+        return self.top
+
+
     def is_empty(self):
         if self.top==0:
             return True
@@ -19,3 +23,9 @@ class Stack:
         if not self.is_empty():
             self.top -= 1
             return self.List[self.top]
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.List[self.top - 1]

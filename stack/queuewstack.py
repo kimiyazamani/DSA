3e93a4147560a00f6479e6d3a4ef93b866560434
kimiya zamani <zamanikimiya.2001@gmail.com>
2023-11-27 16:16:33 +0330

    dequeue

--- stack/queuewstack.py
+++ stack/queuewstack.py
@@ -6,3 +6,14 @@ class QueueUsingStacks:
 
     def enqueue(self, item):
         self.stack1.append(item)
+
+    def dequeue(self):
+        if not self.stack1 and not self.stack2:
+            return None
+
+        if not self.stack2:
+            while self.stack1:
+                self.stack2 += [self.stack1[-1]]
+                self.stack1 = self.stack1[:-1]
+
+        return self.stack2.pop()

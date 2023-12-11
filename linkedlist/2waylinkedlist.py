ef2a19ce9cd7f6116b68cff900ca79fe3cf40f3a
kimiya zamani <zamanikimiya.2001@gmail.com>
2023-12-11 19:35:48 +0330

    defining node & type of linked list classes.

---
+++ linkedlist/2waylinkedlist.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
---
+++ linkedlist/circularlinkedlist.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0

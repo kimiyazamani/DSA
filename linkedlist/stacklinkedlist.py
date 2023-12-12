32efaced1297ddb4439b6afe8e4238956dff2f71
kimiya zamani <zamanikimiya.2001@gmail.com>
2023-12-12 21:45:20 +0330

    push method.

--- linkedlist/stacklinkedlist.py
+++ linkedlist/stacklinkedlist.py
@@ -6,3 +6,8 @@ class Node:
 class Stack:
     def __init__(self):
         self.top = None
+
+    def push(self, data):
+        pushed_node = Node(data)
+        pushed_node.next = self.top
+        self.top = pushed_node

d7df23e8c8076b2bb4e2d8d4196bdcd7562f6756
kimiya zamani <zamanikimiya.2001@gmail.com>
2023-12-13 20:49:04 +0330

    Append method.

--- linkedlist/arraystack.py
+++ linkedlist/arraystack.py
@@ -6,3 +6,13 @@ class Node:
 class Array:
     def __init__(self):
         self.head = None
+
+    def append(self, data):
+        appended_node = Node(data)
+        if self.head is None:
+            self.head = appended_node
+        else:
+            cur = self.head
+            while cur.next:
+                cur = cur.next
+            cur.next = appended_node

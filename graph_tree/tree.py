a1f47811fccaeab7c850397eed3e138531d9bcd1
kimiya zamani <zamanikimiya.2001@gmail.com>
2023-12-26 18:33:12 +0330

    remove method tree

--- graph_tree/tree.py
+++ graph_tree/tree.py
@@ -22,3 +22,31 @@ class BinarySearchTree:
             root.right = self._insert_recursive(root.right, key)
 
         return root
+
+    def _get_min_value(self, root):
+        current = root
+        while current.left is not None:
+            current = current.left
+        return current.key
+
+    def remove(self, key):
+        self.root = self._remove_recursive(self.root, key)
+
+    def _remove_recursive(self, root, key):
+        if root is None:
+            return root
+
+        if key < root.key:
+            root.left = self._remove_recursive(root.left, key)
+        elif key > root.key:
+            root.right = self._remove_recursive(root.right, key)
+        else:
+            if root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+
+            root.key = self._get_min_value(root.right)
+            root.right = self._remove_recursive(root.right, root.key)
+
+        return root

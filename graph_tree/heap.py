82d08c8f03b76ec8c24942f9390a4c2aaa4db260
kimiya zamani <zamanikimiya.2001@gmail.com>
2023-12-26 18:25:10 +0330

    insert method min heap

--- graph_tree/heap.py
+++ graph_tree/heap.py
@@ -41,4 +41,23 @@ class MaxHeap:
         removed_value = self.heap.pop()
         self.heapify_down(index)
         return removed_value
+
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def heapify_up(self):
+        index = len(self.heap) - 1
+        while index > 0:
+            parent_index = (index - 1) // 2
+            if self.heap[index] < self.heap[parent_index]:
+                self.heap[index], self.heap[parent_index] = self.heap[parent_index], self.heap[index]
+                index = parent_index
+            else:
+                break
+
+    def insert(self, value):
+        self.heap.append(value)
+        self.heapify_up()
+
     

42dc678d3116079dfdae3bd467063ff7c557dfcc
kimiya zamani <zamanikimiya.2001@gmail.com>
2023-12-26 18:35:31 +0330

    remove method tree

--- graph_tree/tree.py
+++ graph_tree/tree.py
@@ -10,43 +10,43 @@ class BinarySearchTree:
         self.root = None
 
     def insert(self, key):
-        self.root = self._insert_recursive(self.root, key)
+        self.root = self.insert_recursive(self.root, key)
 
-    def _insert_recursive(self, root, key):
+    def insert_recursive(self, root, key):
         if root is None:
             return TreeNode(key)
 
         if key < root.key:
-            root.left = self._insert_recursive(root.left, key)
+            root.left = self.insert_recursive(root.left, key)
         elif key > root.key:
-            root.right = self._insert_recursive(root.right, key)
+            root.right = self.insert_recursive(root.right, key)
 
         return root
 
-    def _get_min_value(self, root):
+    def get_min_value(self, root):
         current = root
         while current.left is not None:
             current = current.left
         return current.key
 
     def remove(self, key):
-        self.root = self._remove_recursive(self.root, key)
+        self.root = self.remove_recursive(self.root, key)
 
-    def _remove_recursive(self, root, key):
+    def remove_recursive(self, root, key):
         if root is None:
             return root
 
         if key < root.key:
-            root.left = self._remove_recursive(root.left, key)
+            root.left = self.remove_recursive(root.left, key)
         elif key > root.key:
-            root.right = self._remove_recursive(root.right, key)
+            root.right = self.remove_recursive(root.right, key)
         else:
             if root.left is None:
                 return root.right
             elif root.right is None:
                 return root.left
 
-            root.key = self._get_min_value(root.right)
-            root.right = self._remove_recursive(root.right, root.key)
+            root.key = self.get_min_value(root.right)
+            root.right = self.remove_recursive(root.right, root.key)
 
         return root

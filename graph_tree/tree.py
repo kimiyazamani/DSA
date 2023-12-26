cdd81973ca68b40a9092f2f77d1acade9057bced
kimiya zamani <zamanikimiya.2001@gmail.com>
2023-12-26 18:30:19 +0330

    insert method tree

---
+++ graph_tree/tree.py
@@ -0,0 +1,24 @@
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        self.root = self._insert_recursive(self.root, key)
+
+    def _insert_recursive(self, root, key):
+        if root is None:
+            return TreeNode(key)
+
+        if key < root.key:
+            root.left = self._insert_recursive(root.left, key)
+        elif key > root.key:
+            root.right = self._insert_recursive(root.right, key)
+
+        return root

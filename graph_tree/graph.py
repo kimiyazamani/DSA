2fa82c908e063a7ffa05a55bf8ad7a98b9171d47
kimiya zamani <zamanikimiya.2001@gmail.com>
2023-12-26 18:44:24 +0330

    removing vertex & edge method graph

--- graph_tree/graph.py
+++ graph_tree/graph.py
@@ -11,3 +11,14 @@ class Graph:
         self.add_vertex(second_vertex)
         self.graph[first_vertex].append(second_vertex)
         self.graph[second_vertex].append(first_vertex)
+
+    def remove_vertex(self, vertex):
+        if vertex in self.graph:
+            del self.graph[vertex]
+            for adjacent_vertex in self.graph:
+                self.graph[adjacent_vertex] = [v for v in self.graph[adjacent_vertex] if v != vertex]
+
+    def remove_edge(self, first_vertex, second_vertex):
+        if first_vertex in self.graph and second_vertex in self.graph:
+            self.graph[first_vertex] = [v for v in self.graph[first_vertex] if v != second_vertex]
+            self.graph[second_vertex] = [v for v in self.graph[second_vertex] if v != first_vertex]

e74ff7659e3704adeec3d31357a8294c66c99b7c
kimiya zamani <zamanikimiya.2001@gmail.com>
2023-12-26 18:23:09 +0330

    remove method max heap

--- graph_tree/heap.py
+++ graph_tree/heap.py
@@ -15,3 +15,30 @@ class MaxHeap:
     def insert(self, value):
         self.heap.append(value)
         self.heapify_up()
+
+    def heapify_down(self, index):
+        while True:
+            left_child_index = 2 * index + 1
+            right_child_index = 2 * index + 2
+            largest = index
+
+            if left_child_index < len(self.heap) and self.heap[left_child_index] > self.heap[largest]:
+                largest = left_child_index
+
+            if right_child_index < len(self.heap) and self.heap[right_child_index] > self.heap[largest]:
+                largest = right_child_index
+
+            if largest != index:
+                self.heap[index], self.heap[largest] = self.heap[largest], self.heap[index]
+                index = largest
+            else:
+                break
+    def remove(self, value):
+        if value not in self.heap:
+            return
+        index = self.heap.index(value)
+        self.heap[index], self.heap[-1] = self.heap[-1], self.heap[index]
+        removed_value = self.heap.pop()
+        self.heapify_down(index)
+        return removed_value
+    

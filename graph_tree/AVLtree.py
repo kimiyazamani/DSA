ce595f22f41f325807a80731129bf98943f5fbc3
kimiya zamani <zamanikimiya.2001@gmail.com>
2023-12-26 17:49:48 +0330

    height,right & left rotatemethod AVL tree

---
+++ graph_tree/AVLtree.py
@@ -0,0 +1,70 @@
+class AVLNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.height = 1
+
+
+class AVLTree:
+    def __init__(self):
+        self.root = None
+
+    def height(self, node):
+        if not node:
+            return 0
+        return node.height
+
+    def update_height(self, node):
+        if not node:
+            return 0
+        node.height = 1 + max(self.height(node.left), self.height(node.right))
+
+    def balance_factor(self, node):
+        if not node:
+            return 0
+        return self.height(node.left) - self.height(node.right)
+
+    def rotate_right(self, y):
+        x = y.left
+        T2 = x.right
+
+        x.right = y
+        y.left = T2
+
+        self.update_height(y)
+        self.update_height(x)
+
+        return x
+
+    def rotate_left(self, x):
+        y = x.right
+        T2 = y.left
+
+        y.left = x
+        x.right = T2
+
+        self.update_height(x)
+        self.update_height(y)
+
+        return y
+
+    def balance_node(self, node):
+        if not node:
+            return node
+
+        self.update_height(node)
+
+        balance = self.balance_factor(node)
+
+        if balance > 1:
+            if self.balance_factor(node.left) < 0:
+                node.left = self.rotate_left(node.left)
+            return self.rotate_right(node)
+
+        if balance < -1:
+            if self.balance_factor(node.right) > 0:
+                node.right = self.rotate_right(node.right)
+            return self.rotate_left(node)
+
+        return node
